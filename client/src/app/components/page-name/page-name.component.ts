bb319cce03dc08739d61c3776831bbd8fddfc14c
gaspromov <gaspromov@mail.ru>
2021-02-18 03:13:50 +0300

    поправил верстку

--- client/src/app/components/page-name/page-name.component.ts
+++ client/src/app/components/page-name/page-name.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-name',
   templateUrl: './page-name.component.html',
   styleUrls: ['./page-name.component.css']
 })
-export class PageNameComponent implements OnInit {
+export class PageNameComponent implements OnChanges {
   @Input() pageName: string = '';
   @Input() isDataShow: boolean = false;
   userName: string = '';
   id: string = '';
 
-  ngOnInit(): void {
+  ngOnChanges(){
+    this.id = localStorage.getItem('ownerId');
     this.userName = localStorage.getItem('name');
-    this.id = localStorage.getItem('ownerId')
   }
+
+  
 
 
 }
