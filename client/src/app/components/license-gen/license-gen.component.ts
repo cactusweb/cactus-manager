0b61e24de98c14e9f358c9d6e6adb65cb64e62e5
gaspromov <gaspromov@mail.ru>
2021-05-13 14:28:59 +0300

    Поправил прайс в планах

--- client/src/app/components/license-gen/license-gen.component.ts
+++ client/src/app/components/license-gen/license-gen.component.ts
@@ -119,7 +119,7 @@ export class LicenseGenComponent implements OnInit {
 
     this.formLicense = new FormGroup({
       type: new FormControl({value: this.license?.type || 'renewal', disabled: false}, [Validators.required]),
-      price: new FormControl({value: this.license?.payment.price || Number, disabled: false}),
+      price: new FormControl({value: this.license?.payment.price || 0, disabled: false}),
       activations: new FormControl({value: this.license?.activations?.quantity || Number, disabled: false}, [ Validators.required ]),
       expires_in: new FormControl({value: expires_in || '', disabled: false}),
       unbindable: new FormControl({value: this.license?.unbindable || false, disabled: false}),
