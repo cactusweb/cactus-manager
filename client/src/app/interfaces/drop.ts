581a6a9066244394ff6c4ea883d2a2a80c80edda
gaspromov <gaspromov@mail.ru>
2021-06-13 22:07:10 +0300

    исправил интерфейс дропа

--- client/src/app/interfaces/drop.ts
+++ client/src/app/interfaces/drop.ts
@@ -1,12 +1,13 @@
 
 import { License } from './license';
+import { Plan } from './plan';
 
 export interface Drop {
     id: string,
     license: [License],
     owner: string,
     password: string,
-    plan: string,
+    plan: Plan,
     price: 0,
     start_at: 0,
     status: string,
