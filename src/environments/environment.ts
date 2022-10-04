c972dfa2610b7a2b9f4406801951d8b0e57707e2
gaspromov <gaspromov@mail.ru>
2022-10-04 19:07:21 +0300

    bot permission changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiUrl: 'https://test-api.cactusweb.io/api/v2/owner',
   siteDome: 'http://localhost:4200',
   currency: ['USD', 'RUB', 'EUR'],
-  dsBotInvite: 'https://discord.com/api/oauth2/authorize?client_id=840111496947826708&permissions=805308579&redirect_uri=https%3A%2F%2Ftest-api.cactusweb.io%2Fapi%2Fv2%2Fuser%2Fauth%2Ftoken&scope=bot',
+  dsBotInvite: 'https://discord.com/api/oauth2/authorize?client_id=840111496947826708&permissions=8&redirect_uri=https%3A%2F%2Ftest-api.cactusweb.io%2Fapi%2Fv2%2Fuser%2Fauth%2Ftoken&scope=bot',
   primaryOwnerColor: '#5D9BF3'
 };
 
