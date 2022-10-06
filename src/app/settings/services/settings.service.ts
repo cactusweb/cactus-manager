9bd0407c0b257f8bd711eb817779aae10c41b5f0
gaspromov <gaspromov@mail.ru>
2022-10-06 17:43:01 +0300

    added notification for PutOwner request

--- src/app/settings/services/settings.service.ts
+++ src/app/settings/services/settings.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { take, tap } from 'rxjs';
 import { AccountService } from 'src/app/account/services/account.service';
 import { HttpService } from 'src/app/tools/services/http.service';
+import { ToolsService } from 'src/app/tools/services/tools.service';
 import { Requests } from '../const';
 
 @Injectable({
@@ -11,14 +12,16 @@ export class SettingsService {
 
   constructor(
     private http: HttpService,
-    private acc: AccountService
+    private acc: AccountService,
+    private tools: ToolsService
   ) { }
 
   public putSettings( data: Record<string,any> ){
     return this.http.request( Requests['editSelf'], data )
       .pipe(
         take(1),
-        tap( d => this.acc.putOwnerData(d))
+        tap( d => this.acc.putOwnerData(d)),
+        tap(d => this.tools.generateNotification('Account edited', 'success'))
       )
   }
 
