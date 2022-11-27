31bed03a37e1d8a843bbba731713bf4c02f3fe93
gaspromov <gaspromov@mail.ru>
2022-11-28 00:32:55 +0300

    [new] Focus on open addressForm

--- src/app/settings/components/crypto-fieldset/components/address-form/address-form.component.ts
+++ src/app/settings/components/crypto-fieldset/components/address-form/address-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, forwardRef, HostListener, Input, OnInit, Output } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, EventEmitter, forwardRef, HostListener, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MethodControl } from '../../interfaces/method-control';
 
@@ -7,16 +7,22 @@ import { MethodControl } from '../../interfaces/method-control';
   templateUrl: './address-form.component.html',
   styleUrls: ['./address-form.component.scss']
 })
-export class AddressFormComponent implements OnInit {
+export class AddressFormComponent implements OnInit, AfterContentInit {
   @Input() method!: MethodControl
   @Input() address: string = ''
 
   @Output() onSave = new EventEmitter<string>()
   @Output() onClose = new EventEmitter()
 
+  @ViewChild('input') input!: ElementRef;
+
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    setTimeout(() => this.input?.nativeElement?.focus(), 1);
   }
 
   
