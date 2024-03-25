19c14fd6f7e0ed178c4fc17b12fe4f507ab13c0c
gaspromov <gaspromov@mail.ru>
2024-03-25 23:33:23 +0300

    promezh

---
+++ src/app/nft-verification/nft-verification.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cm-nft-verification',
+  templateUrl: './nft-verification.component.html',
+  styleUrls: ['./nft-verification.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NftVerificationComponent {}
