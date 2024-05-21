6286145dcb8eff2a2417a59e3cb7bb37c03c4285
gaspromov <gaspromov@mail.ru>
2024-05-21 11:35:36 +0300

    minor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.cactusweb.io/api/v2/owner',
+  apiUrl: 'https://test-api.cactusweb.io/api/v2/owner',
   siteDome: 'http://localhost:4200',
   currency: ['USD', 'RUB', 'EUR'],
   dsBotInvite:
